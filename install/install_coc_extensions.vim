8a610958588eceeada4a396728e3c4dc38dc18c5
nnsnico <yuji.jampan.blue@gmail.com>
2020-01-15 21:51:45 +0900

    Add to synbolic link of neovim and Add coc.nvim installer

---
+++ install/install_coc_extensions.vim
@@ -0,0 +1,6 @@
+echo execute('CocInstall coc-eslint')
+echo execute('CocInstall coc-yaml')
+echo execute('CocInstall coc-tsserver')
+echo execute('CocInstall coc-rls')
+echo execute('CocInstall coc-metals')
+echo execute('CocInstall coc-json')
--- install/install_vim.sh
+++ install/install_vim.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # install vim
 
-# 1. add synbolic link
+# 1. add vim synbolic link
 ln -s ~/dotfiles/vim/.vimrc ~/
 ln -s ~/dotfiles/vim/coc-settings.json ~/.vim/coc-settings.json
 ln -s ~/dotfiles/vim/.gvimrc ~/
 ln -s ~/dotfiles/vim/.ideavimrc ~/
+
+# (optional) add neovim synbolic link
+ln -s ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # 2. get 'dein.vim' installer, and install in '~/.vim/bundles'
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_vim_installer.sh
