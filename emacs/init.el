43dcecf319f3219c8824a0a5eacfec56b3319973
nnsnico <yuji.jampan.blue@gmail.com>
2020-02-11 00:16:15 +0900

    Fix package config

--- emacs/init.el
+++ emacs/init.el
@@ -3,7 +3,12 @@
 
 ;; init package manager
 (require 'package)
+
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -11,11 +16,15 @@
   (package-install 'use-package))
 
 (eval-when-compile
-  (require 'use-package))
+  (require 'use-package)  
+  (setq use-package-always-ensure t))
+
+;; neotree which-key org-bullets org markdown-mode auto-complete dash material-theme popup rainbow-delimiter
 
 ;; ------------------------- locale and encoding ------------------------------------
 
 ;; locale environment
+(prefer-coding-system 'utf-8)
 (set-locale-environment nil)
 ;; language
 (set-language-environment "Japanese")
@@ -25,49 +34,58 @@
 (set-keyboard-coding-system 'utf-8)
 ;; buffer file encoding
 (set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 
-;; ----------------------------- package config -------------------------------------
+;; ----------------------------- packages config -------------------------------------
+
+;; popup
+(use-package popup)
 
 ;; load material theme
-(load-theme 'material t)
+(use-package material-theme
+  :init
+  (load-theme 'material t))
 
 ;; load autocomplete
-(ac-config-default)
+(use-package auto-complete
+  :init
+  (ac-config-default))
 
 ;; load rainbow delimiters
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(use-package rainbow-delimiters
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
 
 ;; syntax highlight of dash
-(eval-after-load 'dash '(dash-enable-font-lock))
+(use-package dash
+  :after
+  (eval-after-load 'dash '(dash-enable-font-lock)))
 
 ;; neotree
+(use-package all-the-icons)
 (use-package neotree
-  :after
-  projectile
-  :commands
-  (neo-tree-show neotree-hide neotree-dir neotree-find)
-  :custom
-  (neo-theme 'nerd2)
-  :bind
-  ("<f8>" . 'neotree-projectile-toggle)
-  :preface
-  (defun neotree-projectile-toggle ()
-    (interactive)
-    (let ((porject-dir
-	   (ignore-errors
-	     (projectile-project-root)
-	     ))
-	  (file-name (buffer-file-name))
-	  (neo-smart-open t))
-      (if (and (fboundp 'neo-global--window-exists-p)
-	       (neo-global--window-exists-p))
-	  (neotree-hide)
-	(progn
-	  (neotree-show)
-	  (if project-dir
-	      (neotree-dir project-dir))
-	  (if file-name
-	      (neotree-find file-name)))))))
+  :init
+  (setq-default neo-keymap-style 'concise)
+  :config
+  (setq neo-autorefresh nil)
+  (setq neo-create-file-auto-open t)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-show-hidden-files t)
+  (bind-key "C-t" 'neotree-toggle)
+  (bind-key "RET" 'neotree-enter-hide neotree-mode-map)
+  (bind-key "a" 'neotree-hidden-file-toggle neotree-mode-map)
+  (bind-key "<left>" 'neotree-select-up-node neotree-mode-map)
+  (bind-key "<right>" 'neotree-change-root neotree-mode-map))
+
+(defun neo-open-file-hide (full-path &optional arg)
+  (neo-global--select-mru-window arg)
+  (find-file full-path)
+  (neotree-hide))
+
+(defun neotree-enter-hide (&optional arg)
+  (interactive "P")
+  (neo-buffer--execute arg 'neo-open-file-hide 'neo-open-dir))
 
 ;; which-key
 (use-package which-key
@@ -76,19 +94,11 @@
   :hook
   (after-init . which-key-mode))
 
-;; select packages
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d1c7f2db070c96aa674f1d61403b4da1fff2154163e9be76ce51824ed5ca709c" default)))
-n '(package-selected-packages
-   (quote
-    (neotree which-key org-bullets org markdown-mode auto-complete dash material-theme popup rainbow-delimiters))))
-
+;; git-gutter
+(use-package git-gutter
+  :config
+  (global-git-gutter-mode t))
+  
 ;; ------------------------------- os config (mac) ----------------------------------
 
 ;; enable clipboard on osx
@@ -143,7 +153,7 @@ n '(package-selected-packages
 (setq kill-whole-line t)
 
 ;; no show startup message
-(setq inhibit-startup-message t)
+(setq inhibits-startup-message t)
 
 ;; no make backup
 (setq make-backup-files nil)
@@ -159,6 +169,15 @@ n '(package-selected-packages
 
 ;; ignore search case
 (setq case-fold-search t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (all-the-icons which-key neotree dash rainbow-delimiters auto-complete material-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
