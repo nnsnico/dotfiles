07728532e3d7313c234cb60531993794fff3397d
nnsnico <yuji.jampan.blue@gmail.com>
2020-06-21 19:58:05 +0900

    Add python code formatter

--- macos/karabiner-elements/ignore_apps_karabiner.py
+++ macos/karabiner-elements/ignore_apps_karabiner.py
@@ -1,77 +1,85 @@
-'''
+"""
 Karabiner-Elementで一部のアプリに対して効果を無効にする記述を設定ファイル(.json)に追加するスクリプト
 設定ファイルは`~/.config/karabiner/assets/complex_modifications/`配下にあります
-'''
+code formatter: Black
+"""
 
 import json
 from posixpath import os
 
 # 無視したいアプリのアドレスを配列に格納
 # アドレスの確認は`Karabiner-EventViewer`を使用する
-target_bundle_identifiers = [
-    'com.googlecode.iterm2'
-]
+target_bundle_identifiers = ["com.parallels.desktop.console"]
 
 # `~/.config/karabiner/assets/complex_modifications/`内の変更したいファイルを選択
-target_json = 'vi_arrow_bkup.json'
-path = os.path.expandvars('$HOME/.config/karabiner/assets/complex_modifications/')
+target_json = "1531129043.json"
+path = os.path.expandvars("$HOME/.config/karabiner/assets/complex_modifications/")
+
 
 def karabiner_escape(bundle_identifier):
-    with_slash = bundle_identifier.replace('.', '\\.')
-    return '^' + with_slash + '$'
+    with_slash = bundle_identifier.replace(".", "\\.")
+    return "^" + with_slash + "$"
+
 
 def open_json():
     json_data = None
-    with open(path + target_json, 'r') as res:
+    with open(path + target_json, "r") as res:
         json_data = json.load(res)
     return json_data
 
+
 # add_data内のアドレスを`bundle_identifiers`キーに追加する
 # `bundle_identifiers`キーがない場合は追加する
+
+
 def write_bundle_identifiers(json_data, target_bundle_identifiers):
-    with open(path + target_json, 'w') as res:
+    with open(path + target_json, "w") as res:
         # json: root#rules
-        rules = json_data['rules']
+        rules = json_data["rules"]
         for rule in rules:
             # json: root#rules#manipulators
-            manipulators = rule['manipulators']
+            manipulators = rule["manipulators"]
             for manipulator in manipulators:
                 # 'conditions'キー、'type'、'bundle_identifiers'を追加
-                if 'conditions' not in manipulator:
+                if "conditions" not in manipulator:
                     manipulator.update(
                         {
-                            'conditions': [
+                            "conditions": [
                                 {
-                                    'type': 'frontmost_application_unless',
-                                    'bundle_identifiers': []
+                                    "type": "frontmost_application_unless",
+                                    "bundle_identifiers": [],
                                 }
                             ]
                         }
                     )
                 # 追加しようとしている要素がなければ'bundle_identifiers'に追加
                 # json: root#rules#manipulators#conditions
-                for condition in manipulator['conditions']:
+                for condition in manipulator["conditions"]:
                     # json: root#rules#manipulators#conditions#bundle_identifiers
-                    bundle_identifiers = condition['bundle_identifiers']
-                    if (condition['type'] == 'frontmost_application_unless'):
+                    bundle_identifiers = condition["bundle_identifiers"]
+                    if condition["type"] == "frontmost_application_unless":
                         for target in target_bundle_identifiers:
                             # ここをいじればキーを削除するスクリプトに変えれる
-                            if (target not in bundle_identifiers):
-                            # if (target in bundle_identifiers):
+                            if target not in bundle_identifiers:
+                                # if (target in bundle_identifiers):
                                 bundle_identifiers.append(target)
                                 # json: bundle_identifiers.remove(target)
                             else:
-                                print('bundle_identifiers is alreadly exist')
+                                print("bundle_identifiers is alreadly exist")
                                 json.dump(json_data, res, indent=2)
                                 return
         print(json.dumps(json_data, indent=2))
         # エンコード(書き込みモードなのでそのまま上書き)
         json.dump(json_data, res, indent=2)
 
+
 def main():
-    escaped_bundle_identifiers = list(map(lambda x: karabiner_escape(x), target_bundle_identifiers))
+    escaped_bundle_identifiers = list(
+        map(lambda x: karabiner_escape(x), target_bundle_identifiers)
+    )
     json = open_json()
     write_bundle_identifiers(json, escaped_bundle_identifiers)
+
 
 main()
 
