72f1fd868495b4edff333c1fb2fbbaaca3c8d436
nnsnico <y_toyama@fancs.com>
2022-03-17 11:23:11 +0900

    Change path to create symbolic link

--- install/install_vim.sh
+++ install/install_vim.sh
@@ -36,7 +36,7 @@ ln -s ~/dotfiles/vim/.ideavimrc ~/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-ln -s ~/dotfiles/vim/vimrcs/packer/plugins.lua ~/.config/nvim/lua/plugins.lua
+ln -s ~/dotfiles/vim/vimrcs/packer/ ~/.config/nvim/lua/
 
 # 3. install vim plugins
 vim +":call dein#install() | q"
