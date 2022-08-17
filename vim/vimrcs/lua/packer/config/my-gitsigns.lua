2ee137f6ffaaffd93750551161875baa784574cf
nnsnico <y_toyama@fancs.com>
2022-08-17 23:25:10 +0900

    Remove unused code

--- vim/vimrcs/lua/packer/config/my-gitsigns.lua
+++ vim/vimrcs/lua/packer/config/my-gitsigns.lua
@@ -11,9 +11,6 @@ M.config = function()
         vim.keymap.set(mode, l, r, opts)
       end
 
-      -- map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-      -- map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
-
       map('n', ']c', function()
         if vim.wo.diff then return ']c' end
         vim.schedule(function() gs.next_hunk() end)
@@ -25,12 +22,6 @@ M.config = function()
         vim.schedule(function() gs.prev_hunk() end)
         return '<Ignore>'
       end, { expr = true })
-
-      -- map('n', '<Space>ha', ':Gitsigns stage_hunk<CR>')
-      -- map('v', '<Space>ha', ':Gitsigns stage_hunk<CR>')
-      -- map('n', '<Space>hr', ':Gitsigns reset_hunk<CR>')
-      -- map('v', '<Space>hr', ':Gitsigns reset_hunk<CR>')
-      -- map('n', '<Space>hp', '<cmd>Gitsigns preview_hunk<CR>')
 
       map({ 'n', 'v' }, '<Space>ha', ':Gitsigns stage_hunk<CR>')
       map({ 'n', 'v' }, '<Space>hr', ':Gitsigns reset_hunk<CR>')
