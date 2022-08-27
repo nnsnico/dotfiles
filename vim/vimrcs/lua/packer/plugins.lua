4fd70bd6b01b0f6b8769143d46012b0a037b6b93
nnsnico <y_toyama@fancs.com>
2022-08-28 03:24:11 +0900

    Add nvim-metals

--- vim/vimrcs/lua/packer/plugins.lua
+++ vim/vimrcs/lua/packer/plugins.lua
@@ -98,6 +98,30 @@ M.startup = function()
       },
     }
 
+    use {
+      'scalameta/nvim-metals',
+      config = function()
+        local metals_config = require('metals').bare_config()
+        metals_config.settings = {
+          showImplicitArguments = true,
+          excludedPackages = { 'akka.actor.typed.javadsl', 'com.github.swagger.akka.javadsl' }
+        }
+
+        metals_config.capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        metals_config.on_attach = require('packer.config.lsp.my-nvim-lspconfig').on_attach
+
+        local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+        vim.api.nvim_create_autocmd('FileType', {
+          pattern = { 'scala', 'sbt', 'java' },
+          callback = function()
+            require('metals').initialize_or_attach(metals_config)
+          end,
+          group = nvim_metals_group
+        })
+      end,
+      require = { 'nvim-lua/plenary.nvim' }
+    }
+
     ------------------------------- fuzzy finder -------------------------------
 
     use {
