f8e6f58d00be54ef970f869e08fa03c800b87689
nnsnico <yuji.jampan.blue@gmail.com>
2023-01-11 20:22:46 +0900

    Not display title bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,9 +8,10 @@ return {
     { family = 'PlemolJP Console' },
   }),
   font_size                 = 13,
-  window_background_opacity = 0.9,
   custom_block_glyphs       = false,
   enable_tab_bar            = false,
+  window_decorations        = 'NONE|RESIZE',
+  window_background_opacity = 0.9,
   window_padding            = {
     left   = 0,
     right  = 0,
