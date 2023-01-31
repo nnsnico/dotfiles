cfd914934f8499d4f81523e6d834ac3dbbcfe1e1
nnsnico <yuji.jampan.blue@gmail.com>
2023-01-31 13:18:32 +0900

    Remove unused config

--- vim/vimrcs/lua/packer/plugins.lua
+++ vim/vimrcs/lua/packer/plugins.lua
@@ -567,7 +567,6 @@ M.startup = function()
       config = function()
         vim.g.aurora_italic      = 1
         vim.g.aurora_bold        = 1
-        vim.g.aurora_transparent = 1
         vim.g.aurora_darker      = 1
         vim.cmd([[colorscheme aurora]])
       end
