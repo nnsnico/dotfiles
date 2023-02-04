504cd79faa15fcba04c68a8225e63b8b64e4a6a0
nnsnico <yuji.jampan.blue@gmail.com>
2023-02-05 00:21:26 +0900

    Fix crash caused by sign_getdefined being nil

--- vim/vimrcs/lua/packer/config/heirline/components/diagnostic_status.lua
+++ vim/vimrcs/lua/packer/config/heirline/components/diagnostic_status.lua
@@ -5,7 +5,9 @@ local conditions = require('heirline.conditions')
 M.warn = {
   condition = conditions.has_diagnostics,
   init = function(self)
-    self.warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text
+    self.warn_icon = not vim.tbl_isempty(vim.fn.sign_getdefined("DiagnosticSignWarn")) and
+        vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text or
+        ""
     local warn = vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
     self.line = not vim.tbl_isempty(warn) and warn[1].lnum + 1 or 0
   end,
@@ -21,7 +23,9 @@ M.warn = {
 M.error = {
   condition = conditions.has_diagnostics,
   init = function(self)
-    self.error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text
+    self.error_icon = not vim.tbl_isempty(vim.fn.sign_getdefined("DiagnosticSignError")) and
+      vim.fn.sign_getdefined("DiagnosticSignError")[1].text or
+      ""
     local error = vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
     self.line = not vim.tbl_isempty(error) and error[1].lnum + 1 or 0
   end,
