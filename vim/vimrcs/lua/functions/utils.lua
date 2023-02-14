ed97c23c145ec5c2a18ef69f3c2754bc8ba97d6b
nnsnico <yuji.jampan.blue@gmail.com>
2023-02-14 15:01:04 +0900

    delete(utils): unused function

--- vim/vimrcs/lua/functions/utils.lua
+++ vim/vimrcs/lua/functions/utils.lua
@@ -5,14 +5,6 @@ function M.get_current_filename()
   return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':.')
 end
 
----@param method function
-function M.call_safe(method)
-  local ok, err = pcall(method)
-  if not ok then
-    vim.notify(err, vim.log.levels.ERROR)
-  end
-end
-
 ---@param cmd string shell command
 ---@return string command result
 function M.call_process(cmd)
