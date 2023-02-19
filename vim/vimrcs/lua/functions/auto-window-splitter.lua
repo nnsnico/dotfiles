3c52c2cf49a360cf888d61ee40dc30a4f811bbc8
nnsnico <yuji.jampan.blue@gmail.com>
2023-02-20 04:10:30 +0900

    fix(auto-window-splitter): jump accross tabpage only already exist buffer

--- vim/vimrcs/lua/functions/auto-window-splitter.lua
+++ vim/vimrcs/lua/functions/auto-window-splitter.lua
@@ -54,7 +54,7 @@ local function auto_split(filename, windows)
   )
   table.sort(
     window_size_list,
-    function (a, b) return a.area > b.area end
+    function(a, b) return a.area > b.area end
   )
   vim.fn.win_gotoid(window_size_list[1].winid)
 
@@ -83,7 +83,7 @@ function M.auto_jump(filename, lnum, col)
         bufname = vim.fn.fnamemodify(vim.fn.bufname(vim.fn.winbufnr(win)), ':p'),
       }
     end,
-    vim.api.nvim_tabpage_list_wins(0)
+    vim.api.nvim_list_wins()
   )
   local jumpable_windows = get_jumpable_windows(filename, windows)
   if not vim.tbl_isempty(jumpable_windows) then
@@ -93,7 +93,14 @@ function M.auto_jump(filename, lnum, col)
       return
     end
   else
-    auto_split(filename, windows)
+    local current_tabpage_wins = vim.tbl_filter(
+      ---@param v AutoWinSplit.Windows
+      function(v)
+        return vim.tbl_contains(vim.api.nvim_tabpage_list_wins(0), v.winid)
+      end,
+      windows
+    )
+    auto_split(filename, current_tabpage_wins)
     M.move_cursor(0, lnum, col)
   end
 end
