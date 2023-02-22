d4310d57d3f2d97a353959a45ff325bc65fc0c7f
nnsnico <yuji.jampan.blue@gmail.com>
2023-02-22 11:50:29 +0900

    Add custom event for nvim-autopairs

--- vim/vimrcs/lua/plugins/config/my-nvim-cmp.lua
+++ vim/vimrcs/lua/plugins/config/my-nvim-cmp.lua
@@ -12,6 +12,12 @@ M.config = function()
 
   vim.o.completeopt = 'menu,menuone,noselect'
 
+  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  cmp.event:on(
+    'confirm_done',
+    cmp_autopairs.on_confirm_done()
+  )
+
   cmp.setup {
     snippet = {
       expand = function(args)
