b6fea2aabb57eb57c2a3f87fa5ef69c88265e813
nnsnico <yuji.jampan.blue@gmail.com>
2023-09-25 23:35:06 +0900

    chore: remove useless configs

--- vim/vimrcs/lua/plugins/config/skkeleton.lua
+++ vim/vimrcs/lua/plugins/config/skkeleton.lua
@@ -8,8 +8,6 @@ skkeleton.setup = function()
     vim.fn['skkeleton#config']({
       eggLikeNewline        = true,
       globalJisyo           = vim.fn.expand('~/dotfiles/dict/SKK-JISYO'),
-      markerHenkan          = '\u{F7BE} ',
-      markerHenkanSelect    = '\u{F7BE} ',
     })
     vim.fn['skkeleton#register_kanatable']('rom', {
       ['z '] = { '\u{3000}', '' }
@@ -19,13 +17,6 @@ skkeleton.setup = function()
   vim.api.nvim_create_autocmd('User', {
     pattern = 'skkeleton-initialize-pre',
     callback = skkeleton_init,
-  })
-
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'skkeleton-enable-post',
-    callback = function()
-      vim.keymap.del('l', '<CR>', { buffer = true })
-    end,
   })
 
 end
