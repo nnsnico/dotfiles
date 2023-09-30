b85797a61f5a4c8dec291997056add2409d4530b
nnsnico <yuji.jampan.blue@gmail.com>
2023-10-01 01:09:09 +0900

    chore: remove unused arugments

--- vim/vimrcs/basic.lua
+++ vim/vimrcs/basic.lua
@@ -60,70 +60,65 @@ opt.shiftround = true
 
 local keymap = vim.keymap
 
-keymap.set('n', '<Up>',    '<Nop>', {})
-keymap.set('n', '<Down>',  '<Nop>', {})
-keymap.set('n', '<Left>',  '<Nop>', {})
-keymap.set('n', '<Right>', '<Nop>', {})
+keymap.set('n', '<Up>',    '<Nop>')
+keymap.set('n', '<Down>',  '<Nop>')
+keymap.set('n', '<Left>',  '<Nop>')
+keymap.set('n', '<Right>', '<Nop>')
 
 keymap.set('i', '<Up>',    '<Nop>', { remap = true })
 keymap.set('i', '<Down>',  '<Nop>', { remap = true })
 keymap.set('i', '<Left>',  '<Nop>', { remap = true })
 keymap.set('i', '<Right>', '<Nop>', { remap = true })
 
-keymap.set('c', '<C-p>', '<Up>',    {})
-keymap.set('c', '<C-n>', '<Down>',  {})
-keymap.set('c', '<C-b>', '<Left>',  {})
-keymap.set('c', '<C-f>', '<Right>', {})
-keymap.set('c', '<C-a>', '<Home>',  {})
-keymap.set('c', '<C-e>', '<End>',   {})
-keymap.set('c', '<C-d>', '<Del>',   {})
+keymap.set('c', '<C-p>', '<Up>')
+keymap.set('c', '<C-n>', '<Down>')
+keymap.set('c', '<C-b>', '<Left>')
+keymap.set('c', '<C-f>', '<Right>')
+keymap.set('c', '<C-a>', '<Home>')
+keymap.set('c', '<C-e>', '<End>')
+keymap.set('c', '<C-d>', '<Del>')
 
 
 keymap.set('n', 'gs', function() vim.cmd('split') end,  { silent = true })
 keymap.set('n', 'gv', function() vim.cmd('vsplit') end, { silent = true })
 
-keymap.set('n', '<C-l>', function() vim.cmd('normal! gt') end, {})
-keymap.set('n', '<C-h>', function() vim.cmd('normal! gT') end, {})
+keymap.set('n', '<C-l>', function() vim.cmd('normal! gt') end)
+keymap.set('n', '<C-h>', function() vim.cmd('normal! gT') end)
 
-keymap.set('n', '<Space><CR>', 'o<Esc>', {})
+keymap.set('n', '<Space><CR>', 'o<Esc>')
 
-keymap.set({ 'n', 'v' }, 'j', 'gj', {})
-keymap.set({ 'n', 'v' }, 'k', 'gk', {})
+keymap.set({ 'n', 'v' }, 'j', 'gj', { buffer = true })
+keymap.set({ 'n', 'v' }, 'k', 'gk', { buffer = true })
 
-keymap.set({ 'o', 'n', 'x' }, '$', '<Nop>', {})
-keymap.set({ 'o', 'n', 'x' }, '%', '<Nop>', {})
-keymap.set({ 'o', 'n', 'x' }, '^', '<Nop>', {})
+keymap.set({ 'o', 'n', 'x' }, '$', '<Nop>')
+keymap.set({ 'o', 'n', 'x' }, '%', '<Nop>')
+keymap.set({ 'o', 'n', 'x' }, '^', '<Nop>')
 
-keymap.set({ 'o', 'n', 'x' }, 'gh', '^',                            {})
-keymap.set({ 'o', 'n', 'x' }, 'gH', '0',                            {})
-keymap.set({ 'o', 'n', 'x' }, 'gl', '$',                            {})
-keymap.set({ 'o', 'n', 'x' }, 'gL', 'g$',                           {})
+keymap.set({ 'o', 'n', 'x' }, 'gh', '^')
+keymap.set({ 'o', 'n', 'x' }, 'gH', '0')
+keymap.set({ 'o', 'n', 'x' }, 'gl', '$')
+keymap.set({ 'o', 'n', 'x' }, 'gL', 'g$')
 keymap.set({ 'o', 'n' }, 'gp', '<Plug>(MatchitNormalForward)')
 keymap.set({ 'x' }, 'gp', '<Plug>(MatchitVisualForward)')
 
-keymap.set('n', 'ZZ', function() vim.cmd('q') end,  {})
-keymap.set('n', 'ZA', function() vim.cmd('qa') end, {})
-keymap.set('n', 'ZW', function() vim.cmd('wq') end, {})
+keymap.set('n', 'ZZ', function() vim.cmd('q') end)
+keymap.set('n', 'ZA', function() vim.cmd('qa') end)
+keymap.set('n', 'ZW', function() vim.cmd('wq') end)
 
 keymap.set('n', '<Space>m', function()
   vim.cmd('resize')
   vim.cmd('vertical resize')
-end, {})
+end)
 
 keymap.set('n', '<Leader>p', function()
   opt.paste = not opt.paste
-  vim.api.nvim_echo({
-    { 'set paste mode: ' .. tostring(opt.paste) },
-  }, false, {})
-end, {})
+  vim.print('set paste mode: ' .. tostring(opt.paste))
+end)
 
 keymap.set('n', 'tw', function()
   opt.wrap = not opt.wrap
-  print('set wrap line option: ', opt.wrap)
-  vim.api.nvim_echo({
-    { 'set wrap line option: ' .. tostring(opt.wrap) },
-  }, false, {})
-end, {})
+  vim.print('set wrap line option: ' .. tostring(opt.wrap))
+end)
 
 keymap.set('n', 'z.', function()
   vim.cmd('normal! zszH')
