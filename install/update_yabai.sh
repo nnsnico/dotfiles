9df0e01632f7349d107da0652660120668b211eb
nnsnico <yuji.jampan.blue@gmail.com>
2023-10-21 19:15:03 +0900

    chore: remove old shell script

--- install/update_yabai.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-USER=$(whoami)
-YABAI_PATH=$(which yabai)
-CHECKSUM=$(shasum -a 256 "$YABAI_PATH")
-
-TARGET_PATH="/private/etc/sudoers.d/yabai"
-
-echo "$USER ALL = (root) NOPASSWD: sha256:$CHECKSUM --load-sa" > $TARGET_PATH
