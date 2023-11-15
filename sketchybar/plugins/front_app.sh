8742034885371adde79935c9c47f265b97794d1c
nnsnico <yuji.jampan.blue@gmail.com>
2023-11-16 00:39:02 +0900

    feat: add sketchybarrc

---
+++ sketchybar/plugins/front_app.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Some events send additional information specific to the event in the $INFO
+# variable. E.g. the front_app_switched event sends the name of the newly
+# focused application in the $INFO variable:
+# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+
+if [ "$SENDER" = "front_app_switched" ]; then
+  TITLE_YABAI=$(yabai -m query --windows --window | "$HOME"/dotfiles/zsh/.zinit/plugins/jqlang---jq/jq -r '.title')
+  sketchybar --set "$NAME" label="$TITLE_YABAI"
+fi
