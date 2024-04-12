027f5a3a58afa07cf2c2bedcf6325530595ad126
nnsnico <yuji.jampan.blue@gmail.com>
2024-04-12 22:04:53 +0900

    chore(updater): change abbrev for output option

--- install/updater/mycmd/flags.v
+++ install/updater/mycmd/flags.v
@@ -6,7 +6,7 @@ pub const flags = {
 	'output': cli.Flag{
 		flag: cli.FlagType.bool
 		name: 'output'
-		abbrev: 'o'
+		abbrev: 'd'
 		description: 'Prints the result only (not write to the file)'
 	}
 }
