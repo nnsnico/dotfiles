feb7ab0132edcec830182976d3a1b13525c1977c
nnsnico <yuji.jampan.blue@gmail.com>
2024-05-23 17:47:00 +0900

    chore(todo_comments): use `keys` instead of `config`

--- vim/vimrcs/lua/plugins/init.lua
+++ vim/vimrcs/lua/plugins/init.lua
@@ -247,18 +247,24 @@ require('lazy').setup({
   },
   {
     'folke/todo-comments.nvim',
-    config = function()
+    keys = function()
       local todo_comments = require('todo-comments')
-
-      todo_comments.setup()
-
-      vim.keymap.set('n', ']t', function()
-        todo_comments.jump_next()
-      end, { desc = 'Next todo comment' })
-      vim.keymap.set('n', '[t', function()
-        todo_comments.jump_prev()
-      end, { desc = 'Previous todo comment' })
+      return {
+        {
+          ']t', function()
+          todo_comments.jump_next()
+        end,
+          { desc = 'Next todo comment' }
+        },
+        {
+          '[t', function()
+          todo_comments.jump_prev()
+        end,
+          { desc = 'Previous todo comment' }
+        }
+      }
     end,
+    opts = {},
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
