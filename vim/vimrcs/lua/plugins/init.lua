0f0a96dba1783f182a15ff94ec6840bba57b176f
nnsnico <yuji.jampan.blue@gmail.com>
2024-05-23 16:58:27 +0900

    feat: add `todo_comments`

--- vim/vimrcs/lua/plugins/init.lua
+++ vim/vimrcs/lua/plugins/init.lua
@@ -246,6 +246,24 @@ require('lazy').setup({
     opts = {},
   },
   {
+    'folke/todo-comments.nvim',
+    config = function()
+      local todo_comments = require('todo-comments')
+
+      todo_comments.setup()
+
+      vim.keymap.set('n', ']t', function()
+        todo_comments.jump_next()
+      end, { desc = 'Next todo comment' })
+      vim.keymap.set('n', '[t', function()
+        todo_comments.jump_prev()
+      end, { desc = 'Previous todo comment' })
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
+  {
     'stevearc/dressing.nvim',
     event = 'VeryLazy',
     config = function()
