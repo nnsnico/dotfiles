800b83d05e02b811ce18f7bf9d9248eb15baff64
nnsnico <yuji.jampan.blue@gmail.com>
2024-05-25 00:42:24 +0900

    fix: add missing comma

--- vim/vimrcs/lua/plugins/init.lua
+++ vim/vimrcs/lua/plugins/init.lua
@@ -398,7 +398,7 @@ require('lazy').setup({
     config = function()
       require('neoscroll').setup({
         mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zz', 'zt', 'zb' },
-        easing_function = 'circular'
+        easing_function = 'circular',
         hide_cursor = false,
       })
     end
