593a1d606d5db9573db4ff37006135409855e851
nnsnico <yuji.jampan.blue@gmail.com>
2024-07-30 17:58:52 +0900

    feat: replace markdown preview plugin to peek.nvim

--- vim/vimrcs/lua/plugins/init.lua
+++ vim/vimrcs/lua/plugins/init.lua
@@ -599,9 +599,14 @@ require('lazy').setup({
     }
   },
   {
-    'iamcco/markdown-preview.nvim',
-    ft = 'markdown',
-    build = ':call mkdp#util#install()',
+    'toppair/peek.nvim',
+    event = 'VeryLazy',
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
   },
   -------------------------------- color scheme --------------------------------
   {
